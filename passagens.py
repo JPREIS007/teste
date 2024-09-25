e969f6b104ac9e809bf63a70bcacbcc96c267c92
JPREIS007 <ifbaiano4380@gmail.com>
2024-09-25 08:13:19 -0300

    Add files via upload

---
+++ passagens.py
@@ -0,0 +1,41 @@
+tentativas = ["A>C","A>C","A>C","B>D","B>D","B>D","B>D","B>D","A>B","A>B","A>B","A>B","B>C",
+              "B>C","B>C","B>C","B>C","A>D","A>D","A>D","A>D","A>D","A>D","A>D","A>D","A>D",
+              "C>D","C>D","C>D","C>D","C>D","C>D","C>D","C>D","B>D","B>D","B>D","B>D","B>C",
+              "B>C","B>C","A>C","A>B","A>B","B>C","B>C","B>C","A>C","C>D","C>D","C>D","C>D",
+              "C>D","C>D","C>D","B>D","B>D","B>D","B>D","A>B","A>B","A>B","A>B","A>B","A>B",
+              "A>B","A>B","A>B","A>B","A>B","A>C","A>C","A>C","A>C","A>C","A>C","A>C","A>C",
+              "A>C","A>C","A>D","C>D","C>D","C>D","C>D","B>D","B>D","B>D","B>D","B>D","A>B",
+              "A>B","A>C","A>C","A>C","C>D","C>D","A>B","A>B","A>D"]
+
+ab, bc, cd = [], [], []
+poltronas, i = 45, 0
+
+for tentativa in tentativas:
+  i += 1
+  if tentativa == "A>B" and len(ab) < poltronas:
+    ab.append(i)
+
+  if tentativa == "A>C" and len(ab) < poltronas and len(bc) < poltronas:
+    ab.append(i)
+    bc.append(i)
+
+  if tentativa == "A>D" and len(ab) < poltronas and len(bc) < poltronas and len(cd) < poltronas:
+    ab.append(i)
+    bc.append(i)
+    cd.append(i)
+
+  if tentativa == "B>C" and len(bc) < poltronas:
+    bc.append(i)
+
+  if tentativa == "B>D" and len(bc) < poltronas and len(cd) < poltronas:
+    bc.append(i)
+    cd.append(i)
+
+  if tentativa == "C>D" and len(cd) < poltronas:
+    cd.append(i)
+
+  if i in ab or i in bc or i in cd:
+    print(f"{i} - {tentativa} - VENDER\n")
+
+  else:
+    print(f"{i} - {tentativa} - NÃO VENDER\n")
